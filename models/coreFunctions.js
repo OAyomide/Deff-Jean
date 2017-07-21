cff9c64df4bdf8dab0f5e2a5d9edc212e1a1003c
Onigbinde Ayomide <onigbindeayomide@gmail.com>
2017-07-21 02:07:54 +0100

    Update coreFunctions.js

--- models/coreFunctions.js
+++ models/coreFunctions.js
@@ -1,1 +1,37 @@
 
+var request = require('request');
+var URL = process.env.SERVER_URL;
+var access = process.env.FB_ACCESS_TOKEN;
+exports.sendText = function sendTextMessage (recipientId, messageText){
+  var messageData = {
+    recipient : {
+      id: recipientId
+    },
+    message:{
+      text: messageText
+    }
+  };
+  callSendAPI(messageData)
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: access},
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
